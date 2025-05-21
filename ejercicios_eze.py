e5238e47dad1124f1cd324a83cb7c7dfda155112
blopablo <olbap0991@gmail.com>
2025-05-21 15:24:13 -0300

    funciones

--- ejercicios_eze.py
+++ ejercicios_eze.py
@@ -66,7 +66,7 @@ else:
 
 #crear un programa q pida al usuario contraseña y de ser incorrecta q vuelva a pedir
 
-USUARIO_VALIDO = 'Info'
+""" USUARIO_VALIDO = 'Info'
 CONTRASENA_VALIDA = '1234'
 
 while True:
@@ -77,8 +77,59 @@ while True:
         print('acceso concedido!!')
         break
     else:
-        print('usuario o contra')
+        print('usuario o contra') """
     
+# 10 - Ingrese 3 numeros y determine el o los mayores
+
+""" numero1 = int(input('Ingrese un numero: '))
+numero2 = int(input('Ingrese otro numero: '))
+numero3 = int(input('Ingrese un numero mas: '))
+
+if numero1 > numero2 and numero1 > numero3:
+    print('el primer numero ingresado es mayor')
+elif numero2 > numero1 and numero2 > numero3:
+    print('El segundo numero ingresado es mayor')
+elif numero3 > numero1 and numero3 > numero2:
+    print('El tercer numero ingresado es mayor')
+elif numero1 == numero2 and numero1 >= numero3:
+    print(f'El primer numero ingresado: {numero1} y el segundo numero ingresado {numero2} son iguales y mayores')
+elif numero1 == numero3 and numero1 >= numero2:
+    print(f'El primer numero ingresado: {numero1} y el tercer numero ingresado {numero3} son iguales y mayores')
+elif numero3 == numero2 and numero3 >= numero1:
+    print(f'El tercer numero ingresado: {numero3} y el segundo numero ingresado {numero2} son iguales y mayores')
+else:
+    print('los tres numeros son iguales') """
+
+# 11 - suma de dos numeros si son pares
+
+""" print('Jugaremos un juego, si usted ingresa dos numeros pares el programa dira la suma de ellos, en caso contrario dira que los numero no se corresponde')
+num1 = int(input('Ingrese un numero: '))
+num2 = int(input('Ingrese otro numero: '))
+
+if num1 % 2 == 0 and num2 % 2 == 0:
+    print(f'La suma de {num1} + {num2} = {num1 + num2}')
+elif num1 % 2 == 0 and num2 % 2 != 0:
+    print(f'El segundo numero ingresado es un {num2}, por ende no es par')
+else:
+    print(f'El primer numero ingresado es un {num1}, por ende no es par') """
+
+# 12 - Pedir una palabra al usuario y hacer que el programa lo imprima linea por linea
+
+""" palabra = input('ingrese una palabra: ') 
+for letra in palabra:
+    print(letra) """
+
+# 13 - 
+
+n = int(input('ingrese un numero entero: '))
+suma = n * (n + 1) // 2
+print('la suma es: ', suma)
+
+# 14 - tabla de multiplicar
+
+""" print('Bienvenido! aqui obtendras la tabla del 1 al 10 del numero que ingreses!')
+numero = int(input('ingrese un numero entero: '))
+print(f'{numero * 1}, {numero * 2}, {numero * 3}, {numero * 4}, {numero * 5}, {numero * 6}, {numero * 7}, {numero * 8}, {numero * 9}, {numero * 10}') """
 
 
 
---
+++ funciones.py
@@ -0,0 +1,27 @@
+""" def saludar(nombre, apellido):
+    print('hola', nombre, apellido)
+
+saludar("Pablo", "Antunez") """
+
+# suma
+
+""" def sumar(a, b):
+     return a + b
+
+resultado = sumar(7, 3)
+print(f"El resultado es {resultado}") """
+
+num1 =int(input("ingrese un numero entero: "))
+num2 =int(input("ingrese un numero entero: "))
+
+
+
+def dividir(a, b):
+     return a / b
+
+
+resultado = dividir(num1, num2)
+print(f'el resultado es: {resultado:.3f}')
+
+
+# 
